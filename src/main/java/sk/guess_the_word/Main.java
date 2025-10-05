08577d1b4cf45641d6db18826c387f0ba5551831
Saeed Kassab <saeedkassab2000@gmail.com>
2025-10-05 20:51:56 +0200

    Fix bug.

--- src/main/java/sk/guess_the_word/Main.java
+++ src/main/java/sk/guess_the_word/Main.java
@@ -10,12 +10,13 @@ public class Main extends Application {
 
     public void start(Stage stage) {
         View view = new View();
-        Scene scene = new Scene(view, 420, 420);
+        Scene scene = new Scene(view, 380, 380);
         stage.setScene(scene);
         stage.centerOnScreen();
         stage.setTitle("Guess The Word");
         Image icon = new Image("word.png");
         stage.getIcons().add(icon);
+        stage.setResizable(false);
         stage.show();
 
     }
